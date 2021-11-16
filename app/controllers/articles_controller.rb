82a9fd01c9ceada8dffade39e627ef3c78c1c52f
tknlibunao <thea.kaylee.libunao@eee.upd.edu.ph>
2021-11-16 22:30:15 +0800

    added method new and method create

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,15 +1,20 @@
 class ArticlesController < ApplicationController
 	def index
-		#default -> render 'index'
-		#but can use this -> render '<another.html.erb>'
-		#byebug
-		articles = Article.all
-		
-		#Instance variable sa isang class
-		@articles = Article.first
+		@articles = Article.all
 	end
 	
-	def show
+	def new
+		@article = Article.new
+	end
+	
+	def create
+		article_params = params.require(:article).permit(:title, :author, :content)
+		@article = Article.new(article_params)
 		
+		if @article.save
+			redirect_to articles_path
+		else
+			render :new
+		end
 	end
 end
