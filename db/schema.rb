5e4b027c76ac55e26b2c194eb8a3d5ecc8433bb7
tknlibunao <thea.kaylee.libunao@eee.upd.edu.ph>
2022-02-20 15:17:23 +0800

    migrated added columns

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_18_160218) do
+ActiveRecord::Schema.define(version: 2022_02_18_170622) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -39,6 +39,9 @@ ActiveRecord::Schema.define(version: 2022_02_18_160218) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "username"
+    t.string "first_name"
+    t.string "last_name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
